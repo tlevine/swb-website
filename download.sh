f99aeeb185f6c0b8d334c698610b63c6b09d8ed8
Thomas Levine <occurrence@thomaslevine.com>
2013-07-26 10:57:35 -0700

    download the files we are editing

--- download.sh
+++ download.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
+wget http://community.amstat.org/StatisticsWithoutBorders/AboutSWB/Positions
+wget http://community.amstat.org/StatisticsWithoutBorders/AboutSWB/Officers1
+
 wget --recursive http://community.amstat.org/StatisticsWithoutBorders/
 find community.amstat.org/ -type f -regex '^[^.]*$' -execdir mv {} {}.html \;
